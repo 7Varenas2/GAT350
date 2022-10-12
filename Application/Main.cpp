c785f29432a8302ba80049fa25d4fa8f29e2a454
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2022-10-12 15:29:14 -0600

    GLM library added

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -1,15 +1,94 @@
 #include "Engine.h"
 #include <iostream>
 
+float points[] = {
+  -0.5f, -0.5f,  0.0f,
+   -0.5f,  0.1f,  0.0f,
+   0.0f, -0.5f,  0.0f,
+
+   // Second Triangle
+   0.0f, 0.1f, 0.0f,
+   0.0f, -0.5f, 0.0f,
+   -0.5f, 0.1f, 0.0f
+};
+
+neu::Vector3 colors[] = {
+	{0, 0, 1}, // rgb
+	{1, 0, 1},
+	{0, 1, 0},
+	{0, 0, 1},
+	{0, 1, 1},
+	{1, 1, 1},
+};
+
+//const char* vertex_shader =
+//"#version 430 core\n"
+//"in vec3 position;"
+//"void main() {"
+//"  gl_Position = vec4(position, 1.0);" // Changes scale
+//"}";
+//
+//const char* fragment_shader =
+//"#version 430 core\n"
+//"out vec4 color;"
+//"void main() {"
+//"  color = vec4(0.85, 0.0, 0.85, 1.0);"
+//"}";
+
 int main(int argc, char** argv)
 {
 	// DO NOT REMOVE
 	neu::InitializeMemory();
+	neu::SetFilePath("../Assets");
 	 
 	neu::Engine::Instance().Initialize();
 	neu::Engine::Instance().Register();
 
 	neu::g_renderer.CreateWindow("Neumont", 800, 600);
+
+	// Create vertex buffer
+	GLuint pvbo = 0;
+	glGenBuffers(1, &pvbo);
+	glBindBuffer(GL_ARRAY_BUFFER, pvbo);
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(float), points, GL_STATIC_DRAW);
+
+	GLuint cvbo = 0;
+	glGenBuffers(1, &cvbo);
+	glBindBuffer(GL_ARRAY_BUFFER, cvbo);
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(neu::Vector3), colors, GL_STATIC_DRAW);
+
+	// Create vertex array
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, pvbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, cvbo);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// Create shader
+	std::shared_ptr<neu::Shader> vs = neu::g_resources.Get<neu::Shader>("shaders/basic.vert", GL_VERTEX_SHADER);
+	std::shared_ptr<neu::Shader> fs = neu::g_resources.Get<neu::Shader>("shaders/basic.frag", GL_FRAGMENT_SHADER);
+
+
+	//GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	//glShaderSource(vs, 1, &vertex_shader, NULL);
+	//glCompileShader(vs);
+	//GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	//glShaderSource(fs, 1, &fragment_shader, NULL);
+	//glCompileShader(fs);
+
+	// Create program
+	GLuint program = glCreateProgram();
+	glAttachShader(program, fs->m_shader);
+	glAttachShader(program, vs->m_shader);
+	glLinkProgram(program);
+	glUseProgram(program);
 
 	float angle = 0;
 	neu::Vector2 position;
@@ -21,36 +100,8 @@ int main(int argc, char** argv)
 		
 		neu::g_renderer.BeginFrame();
 		
-		if (neu::g_inputSystem.GetKeyState(neu::key_left) == neu::InputSystem::KeyState::Held)
-		{
-			position.x -= neu::g_time.deltaTime;
-		}
-
-		if (neu::g_inputSystem.GetKeyState(neu::key_right) == neu::InputSystem::KeyState::Held)
-		{
-			position.x += neu::g_time.deltaTime;
-		}
-
-		angle += 90.0f * neu::g_time.deltaTime;
-		
-		glPushMatrix();
-
-		glScalef(0.9f, 0.9f, 0.9f);
-		glRotatef(angle, 0, 0, 1);
-		glTranslatef(position.x, position.y, 0);
-
-		glBegin(GL_TRIANGLES);
-
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2f(-1.0f, -1.0f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(1.0f, 1.0f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(1.0f, -1.0f);
-
-		glEnd();
-
-		glPopMatrix();
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+	
 
 		neu::g_renderer.EndFrame();
 	}
---
+++ Build/Assets/Shaders/basic.vert
@@ -0,0 +1,12 @@
+#version 430 core 
+in layout(location = 0) vec3 vposition; // 0
+in layout(location = 1) vec3 vcolor; // 1
+
+out vec3 color;
+
+void main()
+{
+	color = vcolor;
+
+	gl_Position = vec4(vposition, 1.0);
+}
