e02f967e8b0f58e827dba7f724c8f7df99d90fad
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2022-10-25 10:53:25 -0600

    OpenGL Model Loader

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -60,15 +60,19 @@ int main(int argc, char** argv)
 	LOG("Window Initialized...");
 
 	// Create vertex buffer
-	std::shared_ptr<neu::VertexBuffer> vb = neu::g_resources.Get<neu::VertexBuffer>("box");
-	vb->CreateVertexBuffer(sizeof(vertices), 36, vertices);
-	vb->SetAttribute(0, 3, 8 * sizeof(float), 0);
-	vb->SetAttribute(1, 3, 8 * sizeof(float), 3 * sizeof(float));
-	vb->SetAttribute(2, 2, 8 * sizeof(float), 6 * sizeof(float));
+	//std::shared_ptr<neu::VertexBuffer> vb = neu::g_resources.Get<neu::VertexBuffer>("box");
+	//vb->CreateVertexBuffer(sizeof(vertices), 36, vertices);
+	//vb->SetAttribute(0, 3, 8 * sizeof(float), 0);
+	//vb->SetAttribute(1, 3, 8 * sizeof(float), 3 * sizeof(float));
+	//vb->SetAttribute(2, 2, 8 * sizeof(float), 6 * sizeof(float));
+
+	// Create model
+	auto m = neu::g_resources.Get<neu::Model>("Models/ogre.obj");
 
 	// Create material
-	std::shared_ptr<neu::Material> material = neu::g_resources.Get<neu::Material>("Materials/box.mtrl");
+	std::shared_ptr<neu::Material> material = neu::g_resources.Get<neu::Material>("Materials/ogre.mtrl");
 	material->Bind();
+
 
 	material->GetProgram()->SetUniform("tint", glm::vec3{ 1, 0, 0 });
 	material->GetProgram()->SetUniform("scale", 0.5f);
@@ -76,7 +80,7 @@ int main(int argc, char** argv)
 	glm::mat4 model{ 1 };
 	glm::mat4 projection = glm::perspective(45.0f, neu::g_renderer.GetWidth() / (float)neu::g_renderer.GetHeight(), 0.01f, 100.0f);
 
-	glm::vec3 cameraPosition = glm::vec3{ 0, 0, 2 };
+	glm::vec3 cameraPosition = glm::vec3{ 0, 2, 2 };
 	float speed = 1;
 
 	glm::mat4 mx{ 1 };
@@ -100,22 +104,27 @@ int main(int argc, char** argv)
 		if (neu::g_inputSystem.GetKeyState(neu::key_space) == neu::InputSystem::KeyState::Held) cameraPosition.z -= speed * neu::g_time.deltaTime;
 		if (neu::g_inputSystem.GetKeyState(neu::key_enter) == neu::InputSystem::KeyState::Held) cameraPosition.z += speed * neu::g_time.deltaTime;
 
-		model = glm::eulerAngleXYZ(0.0f, neu::g_time.time, 0.0f); // Rotate model
+		model = glm::eulerAngleXYZ(0.0f, neu::g_time.deltaTime, 0.0f); // Rotate model
 		
 		neu::g_renderer.BeginFrame();
 
+		// OpenGL Multiple Objects assignment
 		std::vector<neu::Transform> transforms;
 		for (size_t i = 0; i < 4; i++)
 		{
 			// First row is place, second is angle
-			transforms.push_back({ { neu::random(-1,2), neu::random(-1,2), neu::random(-1,2)} ,  {neu::randomf(90), neu::randomf(90), neu::randomf(90)} });
+			//transforms.push_back({ { neu::random(-1,2), neu::random(-1,2), neu::random(-1,2)} ,  {neu::randomf(90), neu::randomf(90), neu::randomf(90)} });
+			transforms.push_back({ { 1, 1, 1 },  {0, 0, 0} });
 
 			glm::mat4 view = glm::lookAt(cameraPosition, cameraPosition + glm::vec3{ 0, 0, -1 }, glm::vec3{ 0, 1, 0 }); // Moves 
 			glm::mat4 mvp = projection * view * (glm::mat4)transforms[i];
 			material->GetProgram()->SetUniform("mvp", mvp);
 
-			vb->Draw();
+			//vb->Draw();
+			// Model vertex buffer draw
+			m->m_vertexBuffer.Draw();
 		}
+
 
 		neu::g_renderer.EndFrame();
 	}
